6de86cf152d924a8f60ca5e44065eafb51dd5bc7
Tonny Oseko <tonnyoseko0@gmail.com>
2023-02-22 12:31:53 +0300

    create components for the pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { Menu, Button } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { Menu } from "antd";
+import {
+  HomeOutlined,
+  PropertySafetyOutlined,
+  InfoCircleOutlined,
+  PhoneOutlined,
+} from "@ant-design/icons";
 
 const Navbar = () => {
   return (
-    <Menu theme="dark" mode="horizontal" style={{ lineHeight: "64px" }}>
-      <Menu.Item key="1">Home</Menu.Item>
-      <Menu.Item key="2">About</Menu.Item>
-      <Menu.Item key="3">Contact</Menu.Item>
-      <Menu.Item style={{ float: "right" }}>
-        <Button type="primary" icon={<UserOutlined />}>
-          Login
-        </Button>
+    <Menu mode="horizontal">
+      <Menu.Item key="home" icon={<HomeOutlined />}>
+        <a href="/">Home</a>
       </Menu.Item>
-      <Menu.Item>
-        <Button>Signup</Button>
+      <Menu.Item key="properties" icon={<PropertySafetyOutlined />}>
+        <a href="/properties">Properties</a>
+      </Menu.Item>
+      <Menu.Item key="about" icon={<InfoCircleOutlined />}>
+        <a href="/about">About</a>
+      </Menu.Item>
+      <Menu.Item key="contact" icon={<PhoneOutlined />}>
+        <a href="/contact">Contact</a>
       </Menu.Item>
     </Menu>
   );
